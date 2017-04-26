2569e03e7ffca63a3cfa5a3a4a1fb6f2e7f6797e
Ramon Carreras <ramon.carreras@interactiu.cat>
2017-04-26 15:38:29 +0200

    Some improvements to IoniPush.php

--- src/IonicPush.php
+++ src/IonicPush.php
@@ -5,73 +5,55 @@ namespace Tomloprod\IonicPush;
 /**
  * Ionic Push Library v 1.0.3
  * 
- * 
  * @category  Library
  * @author    Tomás L.R (@tomloprod)
  */
 class IonicPush {
 
+    // Constants
+    const METHOD_GET = 'GET';
+    const METHOD_POST = 'POST';
+    const METHOD_DELETE = 'DELETE';
+
+    // API URLS
+    private static $ionicBaseURL = 'https://api.ionic.io';
+    private static $endpoints = array(
+        "sendNotification" => '/push/notifications', // POST
+        "getNotification" => '/push/notifications,:notification_id', // GET
+        "getDeviceInfo" => '/push/tokens/:token_id', // GET
+        "listTokens" => '/push/tokens', // GET
+        "listNotifications" => '/push/notifications', // GET
+        "deleteDevice" => '/push/tokens/:token_id', // DELETE
+    );
+
     // Required for authentication
     private $ionicProfile;
     private $ionicAPIToken;
-    // API URLS
-    private $ionicBaseURL = 'https://api.ionic.io/';
-    private $sendNotification = 'push/notifications'; // POST
-    private $getDeviceInfo = 'push/tokens/:token_id'; // GET
-    private $listTokens = 'push/tokens'; // GET
-    private $listNotifications = 'push/notifications'; // GET
-    private $deleteDevice = 'push/tokens/:token_id'; // DELETE
+
     // Config parameters
     private $pushData = [];
+
     // cURL
     public $timeout = 0;
     public $connectTimeout = 0;
     public $sslVerifypeer = 0;
 
+
+    /**
+     * IonicPush constructor.
+     *
+     * @param $ionicProfile
+     * @param $ionicAPIToken
+     */
     public function __construct($ionicProfile, $ionicAPIToken) {
         $this->ionicProfile = $ionicProfile;
         $this->ionicAPIToken = $ionicAPIToken;
     }
 
-    /**
-     * Set scheduled time for the notification.
-     * @param string $date - Time to start delivery of the notification Y-m-d H:i:s format
-     */
-    public function setScheduled($date) {
-        // Convert date to RFC3339
-        $dateTime = date("c", strtotime($date));
-        $this->pushData["scheduled"] = $dateTime;
-    }
 
     /**
-     * Determines if the message should be delivered as a silent notification.
-     * @param bool $enableSilentNotification
-     */
-    public function setSilentNotification($enableSilentNotification = false) {
-        if ($enableSilentNotification) {
-            $this->pushData["notification"]["android"]["content_available"] = 1;
-            $this->pushData["notification"]["ios"]["content_available"] = 1;
-        } else {
-            unset($this->pushData["notification"]["android"]["content_available"]);
-            unset($this->pushData["notification"]["ios"]["content_available"]);
-        }
-    }
-
-    /**
-     * Custom data.
-     * @param array $payloadData
-     */
-    public function setPayload($payloadData) {
-        if (!is_array($payloadData)) {
-            $payloadData = array($payloadData);
-        }
-        if (count($payloadData) > 0) {
-            $this->pushData['notification']['payload'] = $payloadData;
-        }
-    }
-
-    /**
-     * Notification config.
+     * Set notification config.
+     *
      * @param array $notificationData
      */
     public function setConfig($notificationData) {
@@ -87,45 +69,103 @@ class IonicPush {
     }
 
     /**
-     * Paginated listing of Tokens.
-     * @param array $parameters
-     * @return array
+     * Set custom data.
+     *
+     * @param array $payloadData
      */
-    public function listTokens($parameters) {
-        $getParameters = http_build_query($parameters);
-        return $this->sendRequest("GET", $this->ionicBaseURL . $this->listTokens . "?" . $getParameters, $this->pushData);
+    public function setPayload($payloadData) {
+        if (!is_array($payloadData)) {
+            $payloadData = array($payloadData);
+        }
+        if (count($payloadData) > 0) {
+            $this->pushData['notification']['payload'] = $payloadData;
+        }
     }
 
     /**
+     * Set scheduled time for the notification.
+     *
+     * @param string $date - Time to start delivery of the notification Y-m-d H:i:s format
+     */
+    public function setScheduled($date) {
+        // Convert date to RFC3339
+        $dateTime = date("c", strtotime($date));
+        $this->pushData["scheduled"] = $dateTime;
+    }
+
+    /**
+     * Determines if the message should be delivered as a silent notification.
+     *
+     * @param bool $enableSilentNotification
+     */
+    public function setSilentNotification($enableSilentNotification = false) {
+        if ($enableSilentNotification) {
+            $this->pushData["notification"]["android"]["content_available"] = 1;
+            $this->pushData["notification"]["ios"]["content_available"] = 1;
+        } else {
+            unset($this->pushData["notification"]["android"]["content_available"]);
+            unset($this->pushData["notification"]["ios"]["content_available"]);
+        }
+    }
+
+
+    /**
      * Paginated listing of Push Notifications.
+     *
      * @param array $parameters
      * @return array
      */
     public function listNotifications($parameters) {
         $getParameters = http_build_query($parameters);
-        return $this->sendRequest("GET", $this->ionicBaseURL . $this->listNotifications . "?" . $getParameters, $this->pushData);
+        return $this->sendRequest(self::METHOD_GET, self::$endpoints['listNotifications'] . "?" . $getParameters, $this->pushData);
+    }
+
+    /**
+     * Get information about a specific notification.
+     *
+     * @param string $notificationId - Notification id
+     * @return array
+     */
+    public function getNotification($notificationId) {
+        return $this->prepareNotificationRequest(self::METHOD_GET, $notificationId, self::$endpoints['getNotification']);
+    }
+
+
+    /**
+     * Paginated listing of Tokens.
+     *
+     * @param array $parameters
+     * @return array
+     */
+    public function listTokens($parameters) {
+        $getParameters = http_build_query($parameters);
+        return $this->sendRequest(self::METHOD_GET, self::$endpoints['listTokens'] . "?" . $getParameters, $this->pushData);
     }
 
     /**
      * Get information about a device of a specific device token.
+     *
      * @param string $deviceToken - Device token
      * @return array
      */
     public function getDeviceInfo($deviceToken) {
-        return $this->prepareRequest("GET", $deviceToken, $this->ionicBaseURL . $this->getDeviceInfo);
+        return $this->prepareDeviceRequest(self::METHOD_GET, $deviceToken, self::$endpoints['getDeviceInfo']);
     }
 
     /**
-     * Delete a device related to the device token. 
+     * Delete a device related to the device token.
+     *
      * @param string $deviceToken - Device token
      * @return array
      */
     public function deleteDevice($deviceToken) {
-        return $this->prepareRequest("DELETE", $deviceToken, $this->ionicBaseURL . $this->deleteDevice);
+        return $this->prepareDeviceRequest(self::METHOD_DELETE, $deviceToken, self::$endpoints['deleteDevice']);
     }
+
 
     /**
      * Send push for the indicated device tokens.
+     *
      * @param array $deviceTokens
      * @return array
      */
@@ -134,8 +174,9 @@ class IonicPush {
         return $this->push();
     }
 
-    /** 
+    /**
      * Send push for all registered devices.
+     *
      * @return array
      */
     public function sendPushAll() {
@@ -145,28 +186,44 @@ class IonicPush {
 
     /**
      * Used by "sendPush" and "sendPushAll".
+     *
      * @private
      * @return array
      */
     private function push() {
-        $response = $this->sendRequest("POST", $this->ionicBaseURL . $this->sendNotification, $this->pushData);
+        $response = $this->sendRequest(self::METHOD_POST, self::$endpoints['sendNotification'], $this->pushData);
         return $response;
     }
 
     /**
      * Used by "getDeviceInfo" and "deleteDevice".
+     *
      * @private
      * @param string $method
      * @param string $deviceToken
      * @param string $endPoint
      * @return array
      */
-    private function prepareRequest($method, $deviceToken, $endPoint) {
+    private function prepareDeviceRequest($method, $deviceToken, $endPoint) {
         return $this->sendRequest($method, str_replace(":token_id", md5($deviceToken), $endPoint), $this->pushData);
     }
 
     /**
+     * Used by "getNotification".
+     *
+     * @private
+     * @param $method
+     * @param $notificationId
+     * @param $endPoint
+     * @return array
+     */
+    private function prepareNotificationRequest($method, $notificationId, $endPoint) {
+        return $this->sendRequest($method, str_replace(":notification_id", md5($notificationId), $endPoint), $this->pushData);
+    }
+
+    /**
      * Send requests to the Ionic Push API.
+     *
      * @private
      * @param string $method
      * @param string $endPoint
@@ -175,6 +232,8 @@ class IonicPush {
      */
     private function sendRequest($method, $endPoint, $data) {
         $jsonData = json_encode($data);
+
+        $endPoint = self::$ionicBaseURL . $endPoint;
 
         $ci = curl_init();
 
@@ -194,14 +253,14 @@ class IonicPush {
         curl_setopt($ci, CURLOPT_HEADER, FALSE);
 
         switch ($method) {
-            case "POST":
+            case self::METHOD_POST:
                 curl_setopt($ci, CURLOPT_POST, TRUE);
                 if (!empty($jsonData)) {
                     curl_setopt($ci, CURLOPT_POSTFIELDS, $jsonData);
                 }
                 break;
-            case "DELETE":
-            case "GET":
+            case self::METHOD_GET:
+            case self::METHOD_DELETE:
                 curl_setopt($ci, CURLOPT_CUSTOMREQUEST, $method);
                 if (!empty($jsonData)) {
                     curl_setopt($ci, CURLOPT_POSTFIELDS, $jsonData);
