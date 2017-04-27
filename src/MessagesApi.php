3a46e23aaab5060724a4da56ae750f22271a267f
Ramon Carreras <ramon.carreras@interactiu.cat>
2017-04-27 09:11:51 +0200

    Making library more scalable.

---
+++ src/MessagesApi.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tomloprod\IonicPush;
+
+/**
+ * TODO: Class MessagesApi
+ *
+ * Stores ionic push api methods related to messages collection.
+ * More info: https://docs.ionic.io/api/endpoints/push.html
+ *
+ * @package Tomloprod\IonicPush
+ * @author Tomás L.R (@tomloprod)
+ * @author Ramon Carreras (@ramoncarreras)
+ */
+class MessagesApi extends IonicApiRequest {
+
+    /*private static $endPoints = [
+        'retrieve' => '/push/messages/:message_id', // GET
+        'delete' => '/push/messages/:message_id' //DELETE
+    ];*/
+
+    // TODO: retrieve
+
+    // TODO: delete
+
+}
