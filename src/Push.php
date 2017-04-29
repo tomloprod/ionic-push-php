3ad9c2aa7fc99ed58a7e23b9716bf52181864664
tomloprod <tomloprod@users.noreply.github.com>
2017-04-29 20:15:05 +0200

    Release 1.1.8

--- src/Push.php
+++ src/Push.php
@@ -7,7 +7,7 @@ use Tomloprod\IonicApi\Api\DeviceTokens;
 use Tomloprod\IonicApi\Api\Messages;
 
 /**
- * Ionic API Push Library v 1.1.7
+ * Ionic API Push Library v 1.1.8
  *
  * @package Tomloprod\IonicApi
  * @category  Library
