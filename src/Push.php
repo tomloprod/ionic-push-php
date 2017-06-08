79aff99d158180bc4bc1cad41ecc914e69a7eec7
tomloprod <tomloprod@users.noreply.github.com>
2017-06-08 14:55:46 +0200

    Release 1.5.3

--- src/Push.php
+++ src/Push.php
@@ -9,7 +9,7 @@ use Tomloprod\IonicApi\Api\Messages;
 /**
  * Ionic API Push Library
  *
- * @version 1.5.2
+ * @version 1.5.3
  * @package Tomloprod\IonicApi
  * @category  Library
  * @author  Tomás L.R (@tomloprod)
